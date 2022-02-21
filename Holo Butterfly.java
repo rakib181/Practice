ad32badce2cb046838820435f25d10fd0c35b73e
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-02-22 02:30:41 +0600

    Create Holo Butterfly.java

---
+++ Holo Butterfly.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=2*n-i+1 || j<=2*n;j++) {
+				if(j==i || j==2*n-i+1 || j==1 || j==2*n) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		for(int i=n;i>=1;i--) {
+			for(int j=1;j<=2*n-i+1 || j<=2*n;j++) {
+				if(j==i || j==2*n-i+1 || j==1 || j==2*n) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+	     	}
+	   }
+	}
