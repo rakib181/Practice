b71e7dc956d8914840aa9f82670ae5ed6039ec89
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-02-22 15:10:48 +0600

    Create Reverse Double Diamond.java

---
+++ Reverse Double Diamond.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		int n=sc.nextInt();
+			for(int i=n;i>=1;i--){
+
+			for(int j=n-i;j>=1;j--){
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i-1 || j<=n;j++){
+				if(j==1 || j==2*i-1 ||i==n){
+					System.out.print("*");
+				}else{
+					System.out.print(" ");
+				}
+		}
+		System.out.println();
+	}
+		for(int i=1;i<=n;i++){
+
+			for(int j=n-i;j>=1;j--){
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i-1 || j<=n;j++){
+				if(j==1 || j==2*i-1 ||i==n){
+					System.out.print("*");
+				}else{
+					System.out.print(" ");
+				}
+		}
+		System.out.println();
+	}
+}
+}
