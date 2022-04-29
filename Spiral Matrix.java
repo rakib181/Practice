8a9f4282b605e9480d559bbf40a6cdb0ca3fdd40
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-04-29 19:18:34 +0600

    Update Spiral Matrix.java

--- Spiral Matrix.java
+++ Spiral Matrix.java
@@ -44,3 +44,57 @@ public class SpiralMatrix {
         solve(n,m,arr);
     }
 }
+
+
+
+
+//Practice
+
+
+import java.util.*;
+
+public class SpiralPrac {
+	static void solve(int n,int m,int[][] arr) {
+		int top=0,down=n-1,left=0,right=m-1,dir=0;
+		while(top<=down && left<=right) {
+			if(dir==0) {
+				for(int i=left;i<=right;i++) {
+					System.out.print(arr[top][i]+" ");
+				}
+				dir=1;
+				top++;
+			}else if(dir==1) {
+				for(int i=top;i<=down;i++) {
+					System.out.print(arr[i][right]+" ");
+				}
+				dir=2;
+				right--;
+			}else if(dir==2) {
+				for(int i=right;i>=0;i--) {
+					System.out.print(arr[down][i]+" ");
+				}
+				dir=3;
+				down--;
+			}else {
+				for(int i=down;i>=top;i--) {
+					System.out.print(arr[i][left]+" ");
+				}
+				dir=0;
+				left++;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt(),m=sc.nextInt();
+        int[][] arr=new int[n][m];
+        for(int i=0;i<n;i++) {
+        	for(int j=0;j<m;j++) {
+        		arr[i][j]=sc.nextInt();
+        	}
+        }
+        solve(n,m,arr);
+	}
+}
+
